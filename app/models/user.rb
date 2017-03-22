2fd780a2b756ab837073b10dd91e7b3e2f6d4580
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-03-22 15:43:27 -0600

    organization still coming up as empty array

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,13 +33,38 @@ class User < ApplicationRecord
   end
 
   def organizations
+    # connection = Faraday.new(url: "https://api.github.com") do |faraday|
+    #   faraday.params[:access_token] = self.token
+    # end
+    # response = connection.get "#{User.find_by(token: self.token)}/orgs"
+    # # try = parse(response.body)
+    # byebug
+
     client_id     = ENV["GITHUB_CLIENT_ID"]
     client_secret = ENV["GITHUB_SECRET_KEY"]
     auth          = "?client_id=#{client_id}&client_secret=#{client_secret}"
     parse(Faraday.get("https://api.github.com/users/#{self[:username]}/orgs#{auth}"))
   end
 
-  
+
+  def activity
+    client_id     = ENV["GITHUB_CLIENT_ID"]
+    client_secret = ENV["GITHUB_SECRET_KEY"]
+    auth          = "?client_id=#{client_id}&client_secret=#{client_secret}"
+    parse(Faraday.get("https://api.github.com/users/#{self[:username]}/events#{auth}"))
+  end
+
+  def activity_of_followed
+    client_id     = ENV["GITHUB_CLIENT_ID"]
+    client_secret = ENV["GITHUB_SECRET_KEY"]
+    auth          = "?client_id=#{client_id}&client_secret=#{client_secret}"
+    # parse(Faraday.get("https://api.github.com/users/#{self[:username]}/received_events#{auth}"))
+    self.following.each do |f|
+      return parse(Faraday.get("https://api.github.com/users/#{f[:login]}/events#{auth}"))
+    end
+  end
+
+
   private
 
   def parse(response)
