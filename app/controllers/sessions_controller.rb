c8cd172647325ae73b7aee7f8341af3b5beb6dd4
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-03-21 22:57:31 -0600

    add styling to login page, oauth still successful

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     client_id     = ENV["GITHUB_CLIENT_ID"]
     client_secret = ENV["GITHUB_SECRET_KEY"]
-    
+
     @response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{params[:code]}")
     access_token = @response.body.split(/\W+/)[1]
 
@@ -20,7 +20,11 @@ class SessionsController < ApplicationController
     user.token      = access_token
     user.save
 
-    session[:user_id] = user.id
-    redirect_to dashboard_index_path
+    if user.save
+      session[:user_id] = user.id
+      redirect_to dashboard_index_path
+    else
+      redirect_to root_path
+    end 
   end
 end
