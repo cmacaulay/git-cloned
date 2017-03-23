695cb149c2266009ab64b5648105c7c6942b0fed
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-03-22 20:26:11 -0600

    fixes broken tests, moves service poro to correct file

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,12 +1,47 @@
-# class GithubService
-#
-#   def self.starred
-#     parse(Faraday.get("https://api.github.com/users/#{@current_user.username}/starred"))[:results]
-#   end
-#
-#   private
-#
-#   def parse(response)
-#     JSON.parse(response.body, symbolize_names: true)
-#   end
-# end
+class GithubService
+  attr_reader :token, :connection
+
+  def initialize(user)
+    @connection    = Faraday.new("https://api.github.com") do |faraday|
+      faraday.params[:client_id] = ENV["GITHUB_CLIENT_ID"]
+      faraday.params[:client_secret] = ENV["GITHUB_SECRET_KEY"]
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+  def starred_repos(user)
+    parse(connection.get("users/#{user[:username]}/starred"))
+  end
+
+  def followers(user)
+    parse(connection.get("users/#{user[:username]}/followers"))
+  end
+
+  def following(user)
+    parse(connection.get("users/#{user[:username]}/following"))
+  end
+
+  def repos(user)
+    parse(connection.get("users/#{user[:username]}/repos"))
+  end
+
+  def organizations(user)
+    parse(connection.get("users/#{user[:username]}/orgs"))
+  end
+
+  def activity(user)
+    parse(connection.get("users/#{user[:username]}/events"))
+  end
+
+  def activity_of_followed(user, followed)
+    followed.each do |f|
+      return parse(connection.get("users/#{f[:login]}/events"))
+    end
+  end
+
+  private
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
